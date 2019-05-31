eecbeeafdc0047eba7f257375fa10407e938556b
PurA <51233588+LuciaGacias@users.noreply.github.com>
2019-05-31 13:31:06 +0200

    Add files via upload

---
+++ docs.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PurA Documentation - A documentation page for project</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/website.css">
+    <link rel="stylesheet" href="assets/css/helping.css">
+    <link rel="author" href="humans.txt">
+  </head>
+  <body>
+    <div class="doc__bg"></div>
+    <nav class="header">
+      <h1 class="logo">PurA <span class="logo__thin">Wiki</span></h1>
+      <ul class="menu">
+        <div class="menu__item toggle"><span></span></div>
+        <li class="menu__item"><a href="/index" class="link link--dark"><i class="fa fa-home"></i> Home</a></li>
+      </ul>
+    </nav>
+    <div class="wrapper">
+      <aside class="doc__nav">
+        <ul>
+          <li class="js-btn selected">Get Started</li>
+          <li class="js-btn">Links</li>
+          <li class="js-btn">Contact</li>
+        </ul>
+      </aside>
+      <article class="doc__content">
+        <section class="js-section">
+          <h3 class="section__title">Get Started</h3>
+          <p>Learn how to use the websites created by looking at their documentation by selecting below.</p>
+          <table id="customers">
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Links</th>
+            </tr>
+            <tr>
+              <td>Life Invader</td>
+              <td>A Facebook/Twitter in Roleplay for GTA:V server.</td>
+              <td><a href="/docs.li">Go (FR Docs) ></a></td>
+            </tr>
+            <tr>
+              <td>Soon</td>
+              <td>In the future.</td>
+              <td>Soon</td>
+            </tr>
+            
+          </table>
+          <p>If there are any aids that are no longer valid, please let me know. Enjoy your visit.</p>
+          <!--<h3 class="section__title">Installation</h3>
+          <div class="code__block code__block--notabs">
+            <<<pre class="code code--block">
+              <code>
+                $ scribbler  ——config
+                  {
+                    “encryption”: true, 
+                    “highlighting“: true,
+                    “prettyTable”: false,
+                    “font”: [“Helvetica”, “sans-serif”],
+                    “folder”: “~/Desktop“
+                  }
+              </code>
+            </pre>
+          </div>--->
+          <hr>
+        </section>
+        <section class="js-section">
+          <h3 class="section__title">Links</h3>
+          <p>PurA Links <br>
+            - <a href="/index">Homepage</a> <br>
+            - <a href="/docs">Doc</a> (here)</p>
+          <p>Other Links <br>
+            - <a href="https://life-invader.fivem-rp.eu" target="_blank">Life Invader Homepage</a></p>
+          <hr />
+        </section>
+        <section class="js-section">
+          <h3 class="section__title">Contact</h3>
+          <p>Discord : PurA#4343</p>
+        </section>
+      </article>
+    </div>
+
+    <footer class="footer">Sincerely, PurA.</footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
+    <script src="assets/js/website.js"></script>
+  </body>
+</html>
