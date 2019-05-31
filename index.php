07fb3c0fbc6c56dfc3a1b8e350cc9715ffbef028
PurA <51233588+LuciaGacias@users.noreply.github.com>
2019-05-31 13:27:08 +0200

    website

---
+++ index.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>PurA - Female purity in development.</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/railscasts.min.css">
+      <link rel="stylesheet" href="assets/css/website.css">
+      <link rel="stylesheet" href="assets/css/landing.css">
+      <link rel="author" href="humans.txt">
+  </head>
+  <body>
+    <nav>
+      <div class="logo"></div>
+      <ul class="menu">
+        <div class="menu__item toggle"><span></span></div>
+        <li class="menu__item"><a href="/docs" class="link link--dark"><i class="fa fa-book"></i> Wiki for my website</a></li>
+      </ul>
+    </nav>
+    <div class="hero">
+      <h1 class="hero__title">PurA</h1>
+      <p class="hero__description">Female purity in development.</p>
+    </div>
+    <div class="hero__terminal">
+      <pre>
+        <!-- Place your demo code here -->
+        <code class="shell-session demo">PurA ~ $ </code>
+      </pre>
+    </div>
+    <div class="wrapper">
+      <div class="installation">
+        <!--<h3 class="section__title">Installation</h3>-->
+        <div class="tab__container">
+          <ul class="tab__menu">
+            <li class="tab active" data-tab="life-invader">Life Invader</li>
+            <li class="tab" data-tab="soon1">Soon</li>
+            <li class="tab" data-tab="soon1-2">Soon</li>
+          </ul>
+          <pre class="nohighlight code">
+            <code class="tab__pane active life-invader">Project ~ $  Info : Life Invader | life-invader.fivem-rp.eu</code>
+            <code class="tab__pane soon1">Project ~ $  Info : In the futur</code>
+            <code class="tab__pane soon1-2">Project ~ $  Info : In the futur</code>
+          </pre>
+        </div>
+      </div>
+      <div class="feature">
+        <div class="feature__item">
+          <h3 class="section__title">Fast & Light</h3>
+          <p>Start writing your life with all roleplay websites! No more time wasted navigating and opening up your real life.</p>
+        </div>
+        <div class="feature__item">
+          <h3 class="section__title">Syncing</h3>
+          <p>Data is synchronized on secure servers to avoid data loss and have backups.</p>
+        </div>
+        <div class="feature__item">
+          <h3 class="section__title">Secure</h3>
+          <p>Our servers are secure and the connection to the created sites is done only under SSL.</p>
+        </div>
+      </div>
+      <!--<div class="keybinding">
+        <ul class="keybinding__detail">
+          <h3 class="keybinding__title">Default Keybindings</h3>
+          <li>Quit without saving <span class="keybinding__label">Ctrl+C</span></li>
+          <li>Save <span class="keybinding__label">Cmd+S</span></li>
+          <li>Save and Quit <span class="keybinding__label">Ctrl+D</span></li>
+          <li>Undo <span class="keybinding__label">Cmd+Z</span></li>
+        </ul>
+        <ul class="keybinding__detail">
+          <h3 class="keybinding__title">Markdown Keybindings</h3>
+          <li><span class="keybinding__label">Ctrl+A</span> Insert Link Markdown</li>
+          <li><span class="keybinding__label">Ctrl+I</span> Insert Image Markdown</li>
+          <li><span class="keybinding__label">Ctrl+V</span> Insert YouTube Video</li>
+          <li><span class="keybinding__label">Ctrl+T</span> Insert Table</li>
+        </ul>
+      </div>-->
+      <div class="callout">
+        <p>Read our documentation for use Life Invader and other</p>
+        <a href="/docs" class="button--primary">Let's go !</a>
+      </div>
+    </div>
+    <div class="changelog">
+      <div class="wrapper">
+        <h3 class="section__title">Latest changes on the wiki</h3>
+        <!--<div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v0.7</h4>
+            <small class="changelog__date">10/12/2017</small>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>Improving the writing workflow with better key bindings</li>
+              <li>Design updates</li>
+              <li>SSL Verification for web hooks</li>
+              <li>Render Emoji</li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v0.6</h4>
+            <small class="changelog__date">7/30/2017</small>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>Adding Unicode support</li>
+              <li>Basic text highlighting</li>
+              <li>Fresh Design</li>
+            </ul>
+          </div>
+        </div>--->
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">v0.1</h4>
+            <small class="changelog__date">28/05/2019</small>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>Creation and development</li>
+              <li>Add tutorial for Life Invader</li>
+            </ul>
+          </div>
+        </div>
+        <div class="changelog__callout">
+          <a href="#Soon" class="button--secondary">Checkout Full Log</a>
+        </div>
+      </div>
+    </div>
+    <footer class="footer">Sincerely, PurA.</footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
+    <script src="assets/js/website.js"></script>
+  </body>
+</html>
